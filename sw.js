29416eae9dbb56fbc03c70d0c0d5fcef1936a7fd
infopod-cell <infopod@ya.ru>
2026-08-12 08:10:16 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE='aqua-v1';
+var CORE=['/Akvarium/','/Akvarium/index.html','/Akvarium/app.js','/Akvarium/manifest.webmanifest','/Akvarium/19118369c-0e8c-4f5b-9f05-bd4c79d9c133_1.png'];
+self.addEventListener('install',function(e){
+e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(CORE);}).then(function(){return self.skipWaiting();}));
+});
+self.addEventListener('activate',function(e){
+e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+if(e.request.method!=='GET')return;
+var url=new URL(e.request.url);
+var mine=url.origin===self.location.origin;
+var font=url.hostname.indexOf('fonts.googleapis.com')===0||url.hostname.indexOf('fonts.gstatic.com')===0;
+if(!mine&&!font)return;
+e.respondWith(
+fetch(e.request).then(function(r){
+if(r&&r.ok){var copy=r.clone();caches.open(CACHE).then(function(c){c.put(e.request,copy);});}
+return r;
+}).catch(function(){
+return caches.match(e.request).then(function(m){
+return m||caches.match('/Akvarium/index.html');
+});
+})
+);
+});
